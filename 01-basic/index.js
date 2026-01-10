861f5d879e0115acf7f0dadf23d7a6826cd3da5c
Shahan-07 <iamshahanalam22@gmail.com>
2026-01-10 20:51:39 +0500

    first commit

---
+++ 01-basic/index.js
@@ -0,0 +1,18 @@
+let myarr = [1,2,3,4];
+// myarr.push(6);
+
+// console.log(myarr);
+
+// console.log(myarr.includes(5));  
+// indicates whether the value is present or not
+
+let newArr = ["Apple","mango",'banana'];
+// newArr.splice(2,0,'orange');
+// console.log(newArr);
+
+// let fruits = newArr.slice(1,3)
+// console.log(fruits);
+// console.log(newArr);
+
+let fruits = newArr.includes('mango');
+console.log(fruits);
